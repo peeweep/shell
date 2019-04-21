b9425eb9f441506d3867207f53c0b482d9cfe78d
peeweep <peeweep@0x0.ee>
2019-04-21 16:42:43 +0800

    Update arch_bash.sh

--- arch/arch_bash.sh
+++ arch/arch_bash.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 echo "**************************************************"
-echo "
-[archlinuxcn]
-Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch
-" >>/etc/pacman.conf
-echo "[✔] Adding the tsinghua archlinucn mirrors        "
+if [ `grep "[archlinuxcn]" /etc/pacman.conf  &>> pacman.conf.error.txt` ] ;then
+  cat /etc/pacman.conf
+  echo "[✔] archlinuxcn exist in pacman.conf"
+else
+  echo "[archlinuxcn] " >> /etc/pacman.conf
+  echo "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch">>/etc/pacman.conf
+  echo "[✔] Adding the tsinghua archlinucn mirrors"
+
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 echo "Server = http://mirrors.huaweicloud.com/archlinux/\$repo/os/\$arch" >>/etc/pacman.d/mirrorlist
-echo "[✔] Adding the huaweicloud arch mirrors           "
-echo "[✔] Installing base utils                         "
-
-pacman -S --noconfirm axel clang curl dnsutils dolphin \
-  fcitx fcitx-rime fcitx-configtool fcitx-googlepinyin fcitx-im \
-  git gcc konsole mpv noto-fonts-cjk nano openssh p7zip \
-  python-pip python2 python2-pip sudo xorg plasma sddm\
-  visual-studio-code-bin vim wget yaourt
-echo "[✔] Installing base utils                         "
+echo "[✔] Adding the huaweicloud arch mirrors"
 
 pacman -S --noconfirm archlinuxcn-keyring 
 pacman -Syu --noconfirm haveged
@@ -27,16 +22,23 @@ pacman-key --populate archlinux
 pacman-key --populate archlinuxcn
 echo "[✔] Fix pacman archlinuxcn-keyring in gnupg-2.1   "
 
+pacman -S --noconfirm axel clang curl dnsutils dolphin \
+  fcitx fcitx-rime fcitx-configtool fcitx-googlepinyin fcitx-im \
+  git gcc konsole mpv noto-fonts-cjk nano openssh p7zip \
+  python-pip python2 python2-pip sudo xorg plasma sddm\
+  visual-studio-code-bin vim wget yaourt
+echo "[✔] Installing base utils                         "
+
+
+
 systemctl enable sddm
 systemctl disable netctl
 systemctl enable NetworkManager
 pacman -Syyu
 pacman -S --noconfirm google-chrome
 echo "[✔] Installing google-chrome                      "
-echo "
-GTK_IM_MODULE=fcitx
-QT_IM_MODULE=fcitx
-XMODIFIERS="@im=fcitx"
-" >>~/.xprofile
+echo "GTK_IM_MODULE=fcitx" >>~/.xprofile
+echo "QT_IM_MODULE=fcitx" >>~/.xprofile
+echo "XMODIFIERS=\"@im=fcitx\"" >>~/.xprofile
 echo "[✔] Add fcitx config to xprofile                  "
 echo "**************************************************"
