34d1d5d8ac9ecddddc850af0bf7a879b38547de7
peeweep <peeweep@0x0.ee>
2019-04-22 11:33:21 +0800

    Update arch_xfce4_terminal.sh

--- arch/arch_xfce4_terminal.sh
+++ arch/arch_xfce4_terminal.sh
@@ -4,14 +4,13 @@ git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/Documents/iTerm2-C
 mkdir -p ~/.local/share/xfce4/terminal
 cp -r ~/Documents/iTerm2-Color-Schemes/xfce4terminal/colorschemes ~/.local/share/xfce4/terminal/  
 rm -rf ~/Documents/iTerm2-Color-Schemes
-echo "You can change Konsole Color theme at 
+echo "You can change xfce4-terminal Color theme at 
 Konsole >> Edit >> Preferences >> Color >> Presets,
-
-I recommand [Breeze]."
+I recommand [Calamity]."
 pacman -S --noconfirm nerd-fonts-complete
-echo "You can change Konsole font at
-Konsole -> Settings -> Edit Current Profile -> Appearance -> select font , 
-I recommand [Sauce Code Pro - Medium]."
+echo "You can change xfce4-terminal font at
+Konsole >> Edit >> Preferences >> Appearance,
+I recommand [SauceCodePro Nerd Font Mono Medium]."
 echo "**************************************************"
 pacman -S --noconfirm zsh zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
