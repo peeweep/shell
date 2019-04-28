c3d5d827a659f8ed5ff1129a9fd8bfaf05f54086
peeweep <peeweep@0x0.ee>
2019-04-28 16:50:31 +0800

    Update clone_all_repo.sh

--- linux/clone_all_repo.sh
+++ linux/clone_all_repo.sh
@@ -2,16 +2,14 @@
 username='xxx'
 foldername='xxx'
 folderid='xxx'
-archive_fileName="$foldername/$username$(date +%Y%m%d%H%M%S).7z"
-echo"Create folder: $foldername"
-mkdir -p $foldername
-echo"Enter folder: $foldername"
-cd $foldername/
-echo"get $username\'s library"
+yearfolder="$foldername/$username/$(date +%Y)"
+datefolder= "$foldername/$username/$(date +%Y)/$(date +%Y%m)/$(date +%Y%m%d)"
+archive_fileName="$datefolder/$(date +%Y%m%d%H%M%S).7z"
+mkdir -p $datefolder
+cd $datefolder
 curl -s "https://api.github.com/users/$username/repos?per_page=100" | jq -r ".[].git_url" | xargs -L1 git clone
-echo"archive $archive_fileName"
 7z a $archive_fileName $foldername
-echo"upload $archive_fileName"
-gdrive upload --parent $folderid $archive_fileName
-echo"remove $foldername"
+cd $foldername
+ls | grep -v $(date +%Y) | xargs rm -rf
+gdrive upload --parent $folderid -r $yearfolder
 rm -rf $foldername
