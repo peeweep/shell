a2e73e10c2251f1dea5ea7b8af7d4abd6d2d83a8
peeweep <peeweep@0x0.ee>
2019-05-09 12:28:55 +0800

    Fix: wrong  folder name

--- linux/backup_rssbot.sh
+++ linux/backup_rssbot.sh
@@ -3,6 +3,6 @@ curDate=$(date +%Y%m%d%H%M%S)
 pathname='/root'
 folder_name='rssbot'
 folder_id='16P2064P2d2SPGaSUfF4wS9JNqJ_MRAZv'
-7z a "$pathname"/"$curDate".7z "$pathname"/"$test" 
+7z a "$pathname"/"$curDate".7z "$pathname"/"$folder_name" 
 gdrive upload "$pathname"/"$curDate".7z  -p "$folder_id"
 rm "$pathname"/"$curDate".7z
