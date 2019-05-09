ff21bd44735fdd7a08bc31cf9fded8372b4d704c
peeweep <peeweep@0x0.ee>
2019-05-09 12:19:33 +0800

    Update and rename 7z_backup.sh to backup_rssbot.sh

--- linux/backup_rssbot.sh
+++ linux/backup_rssbot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curDate=$(date +%Y%m%d%H%M%S)
-pathname='file_path'
-folder_name='folder_name'
-folder_id='google_drive_folder_id'
+pathname='/root'
+folder_name='rssbot'
+folder_id='16P2064P2d2SPGaSUfF4wS9JNqJ_MRAZv'
 7z a "$pathname"/"$curDate".7z "$pathname"/"$test" 
 gdrive upload "$pathname"/"$curDate".7z  -p "$folder_id"
 rm "$pathname"/"$curDate".7z
