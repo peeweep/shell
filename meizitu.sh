8bc35d45eddc30f28320d53ef1ab163de6c79be5
peeweep <peeweep@0x0.ee>
2019-05-21 23:33:39 +0800

    Create meizitu.sh

---
+++ meizitu.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# required jq
+# for i in {1..264}
+for i in {1..2} ; do ;  curl -s http://adr.meizitu.net/wp-json/wp/v2/posts?page=$i&per_page=20 | jq -r '.[] | .thumb_src' | xargs -IX curl -s -O X ; done
