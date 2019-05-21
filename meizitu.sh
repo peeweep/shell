4872e434e9a2a801fa8b04f0751207b2bdd5cf2e
peeweep <peeweep@0x0.ee>
2019-05-21 23:56:16 +0800

    Update meizitu.sh

--- meizitu.sh
+++ meizitu.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # required jq
 # for i in {1..264}
-for i in {1..2} ; do ; curl -s http://adr.meizitu.net/wp-json/wp/v2/posts?page=$i&per_page=20 | jq -r '.[] | .thumb_src' | xargs -IX curl -s -O X ; mkdir $i ; mv *.jpg  $i ; echo 第$i页下载完成 ; done
+for i in {1..2} ; do 
+  curl -s http://adr.meizitu.net/wp-json/wp/v2/posts\?page\=$i\&per_page\=20 | jq -r '.[] | .thumb_src' | xargs -IX curl -s -O X
+  mkdir $i && mv *.jpg $i
+  echo FinishPage$i
+done
