838863707dffe144ccd83b05423360570dbc24e6
peeweep <peeweep@0x0.ee>
2019-06-01 08:45:39 +0800

    Update: sudo update

--- arch/arch_konsole.sh
+++ arch/arch_konsole.sh
@@ -1,21 +1,20 @@
 git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/Documents/iTerm2-Color-Schemes
-cp ~/Documents/iTerm2-Color-Schemes/konsole/*.colorscheme /usr/share/konsole
+sudo cp ~/Documents/iTerm2-Color-Schemes/konsole/*.colorscheme /usr/share/konsole
 rm -rf ~/Documents/iTerm2-Color-Schemes
 echo "You can change Konsole Color theme at 
 Konsole -> Settings -> Edit Current Profile -> Appearance -> select color template ,
 I recommand [Breeze]."
 
-pacman -S --noconfirm nerd-fonts-complete
+sudo pacman -S nerd-fonts-complete
 echo "You can change Konsole font at
 Konsole -> Settings -> Edit Current Profile -> Appearance -> select font , 
 I recommand [Sauce Code Pro - Medium]."
 
-pacman -S --noconfirm zsh zsh-autosuggestions
+sudo pacman -S zsh zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /bin/zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-before = 'ZSH_THEME="robbyrussell"'
-after = '# ZSH_THEME="robbyrussell"
+before='ZSH_THEME="robbyrussell"'
+after='# ZSH_THEME="robbyrussell"
 ZSH_THEME="powerlevel9k/powerlevel9k"
 # Left
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir dir_writable vcs)
@@ -26,9 +25,9 @@ POWERLEVEL9K_MODE="nerdfont-complete"
 
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 '
-sed -i "s/$before/$after/g" ~/.zshrc
+sed -i "s/${before}/${after}/g" ~/.zshrc
 
 
-pacman -S --noconfirm vim
+sudo pacman -S  vim
 curl -sLf https://spacevim.org/install.sh | bash
 vim
