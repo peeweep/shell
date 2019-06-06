3a72e7d8e7b5f7ebf943874c6a9f04b212f60176
peeweep <peeweep@0x0.ee>
2019-06-06 15:31:12 +0800

    Update: bjtu mirror / package

--- arch/arch_base.sh
+++ arch/arch_base.sh
@@ -2,12 +2,12 @@
 {
   echo "[archlinuxcn]" 
   echo "SigLevel = Optional TrustedOnly"
-  echo "Server = http://mirrors.163.com/archlinuxcn/\$arch"
+  echo "Server = http://mirror.bjtu.edu.cn/archlinuxcn/\$arch"
 } | sudo tee -a /etc/pacman.conf
 echo "[✔] Adding archlinuxcn mirrors"
 
 sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-echo "Server = http://mirrors.163.com/archlinux/\$repo/os/\$arch" >> sduo tee -a /etc/pacman.d/mirrorlist
+echo "Server = http://mirror.bjtu.edu.cn/archlinux/\$repo/os/\$arch" >> sduo tee -a /etc/pacman.d/mirrorlist
 echo "[✔] Adding the huaweicloud arch mirrors"
 
 pacman -Syu archlinuxcn-keyring
@@ -23,7 +23,7 @@ echo "archlinuxcn-keyring installed"
 #echo "[✔] Fix pacman archlinuxcn-keyring in gnupg-2.1"
 
 sudo pacman -Syu axel chromium clang curl dnsutils fcitx fcitx-rime fcitx-configtool fcitx-im \
-  flameshot gcc gdb git mpv nano noto-fonts-cjk openssh p7zip python-pip python2 python2-pip \
+  flameshot gcc gdb git gvim mpv nano noto-fonts-cjk openssh p7zip python-pip python2 python2-pip \
   shellcheck shfmt telegram-desktop ttf-opensans unrar vim visual-studio-code-bin wget yay
 echo "[✔] Installing base utils"
 
