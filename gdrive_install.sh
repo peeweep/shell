dd8724dc55e770ce6784b908cab96839806d0519
peeweep <peeweep@0x0.ee>
2019-06-06 16:12:39 +0800

    Update: Team drive support for gdrive

--- gdrive_install.sh
+++ gdrive_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gdrive_linux_x64='http://docs.google.com/uc?id=1Ej8VgsW5RgK66Btb9p74tSdHMH3p4UNb&export=download'
+gdrive_linux_x64="https://drive.google.com/uc?id=1iIjBty1FKxdGvyc4GATngwgbzQdPYz2p&export=download"
 sudo wget -N --no-check-certificate "${gdrive_linux_x64}" -O /usr/bin/gdrive 
 sudo chmod +x /usr/bin/gdrive
 
