4a06dca4cdf8047e2d5c17fb1cf1b8172da1a6c3
peeweep <peeweep@0x0.ee>
2019-06-07 16:14:14 +0800

    Update: Collet catbox/gdrive/vimcn to bin_init.sh

---
+++ bin_init/bin_init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+func() {
+  sudo wget -N --no-check-certificate "${1}" -O /usr/bin/"${2}"
+  sudo chmod +x /usr/bin/"${2}"
+}
+
+catbox="https://files.catbox.moe/ct8vm8"
+func "${catbox}" "catbox"
+
+gdrive="https://drive.google.com/uc?id=1iIjBty1FKxdGvyc4GATngwgbzQdPYz2p&export=download"
+func "${gdrive}" "gdrive"
+
+vimcn="https://img.vim-cn.com/4b/d623a900d9e3c1972d0ecd91e5e922c3336030.bat"
+func "${vimcn}" "vimcn"
