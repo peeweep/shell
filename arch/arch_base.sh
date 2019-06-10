3150771081177950e1191a4cd9f1f34c3f9f3ebb
peeweep <peeweep@0x0.ee>
2019-06-10 18:48:50 +0800

    Add: chaotic-aur

--- arch/arch_base.sh
+++ arch/arch_base.sh
@@ -2,13 +2,18 @@
 {
   echo "[archlinuxcn]"
   echo "SigLevel = Optional TrustedOnly"
-  echo "Server = http://mirror.bjtu.edu.cn/archlinuxcn/\$arch"
+  echo "Server = http://mirrors.163.com/archlinuxcn/\$arch"
+  echo "[chaotic-aur]"
+  echo "Server = http://lonewolf-builder.duckdns.org/\$repo/x86_64"
+  echo "#Server = http://aur.03.rw/chaotic-aur/\$repo/x86_64"
+  echo "#Server = http://chaotic-aur.03.rw/\$repo/x86_64"
+  echo "#[disastrousaur]"
+  echo "#Server = https://mirror.repohost.de/\$repo/\$arch"
 } | sudo tee -a /etc/pacman.conf
-echo "[✔] Adding archlinuxcn mirrors"
+echo "[✔] Adding archlinux third repositories"
 
 sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-echo "Server = http://mirror.bjtu.edu.cn/archlinux/\$repo/os/\$arch" | sudo tee -a /etc/pacman.d/mirrorlist
-echo "[✔] Adding the huaweicloud arch mirrors"
+echo "Server = http://mirrors.163.com/archlinux/\$repo/os/\$arch" | sudo tee -a /etc/pacman.d/mirrorlist
 
 sudo pacman -Syu archlinuxcn-keyring
 echo "archlinuxcn-keyring installed"
@@ -21,9 +26,12 @@ echo "archlinuxcn-keyring installed"
 #pacman-key --populate archlinuxcn
 #pacman -S --noconfirm archlinuxcn-keyring
 #echo "[✔] Fix pacman archlinuxcn-keyring in gnupg-2.1"
+sudo pacman-key --keyserver keys.mozilla.org -r 3056513887B78AEB
+sudo pacman-key --lsign-key 3056513887B78AEB
+echo "chaotic-aur Verified"
 
 sudo pacman -Syu axel chromium clang curl dnsutils fcitx fcitx-rime fcitx-configtool fcitx-im \
-  flameshot gcc gdb git gvim mpv nano noto-fonts-cjk openssh p7zip python-pip python2 python2-pip \
+  flameshot gcc gdb git mpv nano noto-fonts-cjk openssh p7zip python-pip python2 python2-pip \
   shellcheck shfmt telegram-desktop ttf-opensans unrar vim visual-studio-code-bin wget yay
 echo "[✔] Installing base utils"
 
