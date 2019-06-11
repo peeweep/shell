ba231e5587fa38ed8f57a7e949584f780d4e2575
peeweep <peeweep@0x0.ee>
2019-06-11 16:28:24 +0800

    Update: shellcheck/shfmt mojave_base and add packages

--- mojave_base.sh
+++ mojave_base.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install wget curl axel git gcc vim 
-brew cask install google-chrome iterm2 visual-studio-Code
+
+brew install axel clang-format curl gcc git gnupg p7zip shellcheck shfmt vim wget
+brew cask install chromium iina iterm2 squirrel visual-studio-Code telegram-desktop
+
 git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/Documents/iTerm2-Color-Schemes
 echo "You can import  Afterglow color template in Preferences - Profiles - Color Presets,
 I recommand [Afterglow]."
@@ -11,11 +14,12 @@ echo "You can setting font in Preferences -> Profiles -> Text,
 I recommand [Sauce Code Pro Nerd Font Complete]"
 
 brew install zsh zsh-autosuggestions
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-before = 'ZSH_THEME="robbyrussell"'
-after = '# ZSH_THEME="robbyrussell"
+
+before='ZSH_THEME="robbyrussell"'
+after='alias farsee="curl -F \"c=@-\" \"http://fars.ee/\""
+# ZSH_THEME="robbyrussell"
 ZSH_THEME="powerlevel9k/powerlevel9k"
 # Left
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir dir_writable vcs)
@@ -23,8 +27,7 @@ POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir dir_writable vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs history ram load time)
 # for nerd-font
 POWERLEVEL9K_MODE="nerdfont-complete"
-
-source  /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 '
 sed -i "s/$before/$after/g" ~/.zshrc
 
