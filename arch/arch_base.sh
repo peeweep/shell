949782a4cd275c39170679046951b26894f08665
peeweep <peeweep@0x0.ee>
2019-06-13 16:15:04 +0800

    Update: arch mirrors

--- arch/arch_base.sh
+++ arch/arch_base.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 {
-  echo "[archlinuxcn]"
-  echo "SigLevel = Optional TrustedOnly"
-  echo "Server = http://mirrors.163.com/archlinuxcn/\$arch"
-  echo "[chaotic-aur]"
-  echo "Server = http://lonewolf-builder.duckdns.org/\$repo/x86_64"
-  echo "#Server = http://aur.03.rw/chaotic-aur/\$repo/x86_64"
-  echo "#Server = http://chaotic-aur.03.rw/\$repo/x86_64"
-  echo "#[disastrousaur]"
-  echo "#Server = https://mirror.repohost.de/\$repo/\$arch"
+	echo "[archlinuxcn]"
+	echo "SigLevel = Optional TrustedOnly"
+	echo "Server = https://mirrors.sjtug.sjtu.edu.cn/archlinux-cn/\$arch"
+	echo "#[chaotic-aur]"
+	echo "#Server = https://lonewolf.pedrohlc.com/\$repo/x86_64"
+	echo "#Server = http://aur.03.rw/chaotic-aur/\$repo/x86_64"
+	echo "#Server = http://chaotic-aur.03.rw/\$repo/x86_64"
+	echo "[disastrousaur]"
+	echo "Server = https://mirror.repohost.de/\$repo/\$arch"
 } | sudo tee -a /etc/pacman.conf
 echo "[✔] Adding archlinux third repositories"
 
 sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-echo "Server = http://mirrors.163.com/archlinux/\$repo/os/\$arch" | sudo tee -a /etc/pacman.d/mirrorlist
+echo "Server = https://mirrors.huaweicloud.com/archlinux/\$repo/os/\$arch" | sudo tee -a /etc/pacman.d/mirrorlist
 
 sudo pacman -Syu archlinuxcn-keyring
 echo "archlinuxcn-keyring installed"
@@ -31,14 +31,14 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 echo "chaotic-aur Verified"
 
 sudo pacman -Syu axel chromium clang curl dnsutils fcitx fcitx-rime fcitx-configtool fcitx-im \
-  flameshot gcc gdb git mpv nano net-tools noto-fonts-cjk openssh p7zip python-pip python2 python2-pip \
-  shellcheck shfmt telegram-desktop-bin ttf-opensans unrar vim visual-studio-code-bin wget yay
+	flameshot gcc gdb git mpv nano net-tools noto-fonts-cjk openssh p7zip python-pip python2 python2-pip \
+	shellcheck shfmt telegram-desktop-bin ttf-opensans unrar vim visual-studio-code-bin wget yay
 echo "[✔] Installing base utils"
 
 {
-  echo "GTK_IM_MODULE=fcitx"
-  echo "QT_IM_MODULE=fcitx"
-  echo "XMODIFIERS=\"@im=fcitx\""
+	echo "GTK_IM_MODULE=fcitx"
+	echo "QT_IM_MODULE=fcitx"
+	echo "XMODIFIERS=\"@im=fcitx\""
 } | tee -a ~/.xprofile
 echo "[✔] Add fcitx config to xprofile"
 
