bc664b18c17089b0826231119e3925d4bd038885
peeweep <peeweep@0x0.ee>
2019-06-27 07:47:03 +0800

    refactor(mojave_base): retry while download failed

--- mojave_base.sh
+++ mojave_base.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install axel clang-format curl gcc git gnupg p7zip shellcheck shfmt vim wget
-brew cask install google-chrome iina iterm2 squirrel visual-studio-Code telegram
+homebrew_install() {
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew fetch --retry axel clang-format curl gcc git gnupg p7zip shellcheck shfmt wget
+    brew install axel clang-format curl gcc git gnupg p7zip shellcheck shfmt wget
+    brew cask install google-chrome iina iterm2 visual-studio-Code telegram
+}
 
-git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/Documents/iTerm2-Color-Schemes
-echo "You can import  Afterglow color template in Preferences - Profiles - Color Presets,
+iterm_scheme() {
+    git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/Documents/iTerm2-Color-Schemes
+    echo "You can import  Afterglow color template in Preferences - Profiles - Color Presets,
 I recommand [Afterglow]."
+}
 
-brew tap caskroom/fonts
-brew cask install font-sourcecodepro-nerd-font
-echo "You can setting font in Preferences -> Profiles -> Text,
+homebrew_nerd() {
+    brew tap caskroom/fonts
+    brew cask install font-sourcecodepro-nerd-font
+    echo "You can setting font in Preferences -> Profiles -> Text,
 I recommand [Sauce Code Pro Nerd Font Complete]"
+}
 
-brew install zsh zsh-autosuggestions
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+homebrew_omz_install() {
+    brew install zsh zsh-autosuggestions
+    brew cleanup
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+}
 
-before='ZSH_THEME="robbyrussell"'
-after='alias farsee="curl -F \"c=@-\" \"http://fars.ee/\""
+homebrew_omz_setup() {
+    before='ZSH_THEME="robbyrussell"'
+    after='alias farsee="curl -F \"c=@-\" \"http://fars.ee/\""
 # ZSH_THEME="robbyrussell"
 ZSH_THEME="powerlevel9k/powerlevel9k"
 # Left
@@ -29,7 +41,23 @@ POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs history ram load time
 POWERLEVEL9K_MODE="nerdfont-complete"
 source  /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 '
-sed -i "s/$before/$after/g" ~/.zshrc
+    sed -i "s/$before/$after/g" ~/.zshrc
+}
 
-curl -sLf https://spacevim.org/install.sh | bash
-vim
+homebrew_omz() {
+    homebrew_nerd
+    homebrew_omz_install
+    homebrew_omz_setup
+}
+
+spacevim() {
+    brew fetch --retry vim
+    brew install vim
+    curl -sLf https://spacevim.org/install.sh | bash
+    vim
+}
+
+homebrew_install
+iterm_scheme
+homebrew_omz
+spacevim
